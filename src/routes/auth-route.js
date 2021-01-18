160e9af4e69306633ce2b5dcc3607e3e740b834f
Indra Pramana <indra.pramana@integra.co.id>
2021-01-18 16:55:14 +0700

    add new functions and express-validation -rev

--- src/routes/auth-route.js
+++ src/routes/auth-route.js
@@ -1,8 +1,8 @@
 import express from 'express'
 import validator from 'express-validator'
 import {
-	frontend, signin,
-	signup, refreshToken,
+	signin, signup,
+	refreshToken,
 	forgotPass
 } from '../controllers/auth-controller.js'
 import { auth } from '../middlewares/auth.js'
@@ -10,7 +10,6 @@ import { auth } from '../middlewares/auth.js'
 const authRouter = express.Router()
 const { body } = validator
 
-authRouter.get('/', auth, frontend)
 authRouter.post('/signin',
 	body('email').isEmail().normalizeEmail(),
 	body('password').isLength({ min: 6 }), signin)
@@ -20,5 +19,5 @@ authRouter.post('/signup',
 authRouter.get('/refresh-token', auth, refreshToken)
 authRouter.post('/forgot-password',
 	body('email').isEmail().normalizeEmail(), forgotPass)
-	
+
 export default authRouter
